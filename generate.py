11f21c709de40026d4da874cc8d08a4774d56e36
Husniddin <uzbekfreedom@gmail.com>
2024-01-03 20:20:55 +0500

    env

--- generate.py
+++ generate.py
@@ -1,9 +1,7 @@
-import openai
+import os
 from openai import OpenAI
+api_key = os.environ["OPEN_AI_KEY"] = "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"
 client = OpenAI()
-
-openai.api_key = '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'
-
 def generate_post(channel_name: str, channel_topic: str, post_title: str, post_description: str) -> str:
     # OpenAI GPT-3 ga so'rov yuborish
     prompt = f"I am an AI developed by OpenAI. I have been asked to generate a post for a Telegram channel named {channel_name}, which focuses on the topic of {channel_topic}. The post should be titled {post_title} and should align with the following description: {post_description}. Please generate a post that fits this description and is engaging for the channel's audience."
